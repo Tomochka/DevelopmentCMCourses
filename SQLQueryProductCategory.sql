1baa9487ff3e179930c2d9ddcfaba60eb75f28d5
Tomochka <33438499+Tomochka@users.noreply.github.com>
2020-04-24 00:20:27 +0300

    Update SQLQueryProductCategory.sql

--- SQLQueryProductCategory.sql
+++ SQLQueryProductCategory.sql
@@ -1,4 +1,5 @@
 use Product_Category
 go
 
-select dbo.Product.product, dbo.Product_Category.category from dbo.Product left join dbo.Product_Category ON dbo.Product.id = dbo.Product_Category.product
+select dbo.Product.product, dbo.Product_Category.category 
+from dbo.Product left join dbo.Product_Category ON dbo.Product.id = dbo.Product_Category.product
