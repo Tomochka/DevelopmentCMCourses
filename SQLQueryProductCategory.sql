cdb7b67cfcea5b1535a1035d4b9b5b8440f87cbe
Tomochka <33438499+Tomochka@users.noreply.github.com>
2020-04-24 00:20:00 +0300

    Add files via upload

---
+++ SQLQueryProductCategory.sql
@@ -0,0 +1,4 @@
+use Product_Category
+go
+
+select dbo.Product.product, dbo.Product_Category.category from dbo.Product left join dbo.Product_Category ON dbo.Product.id = dbo.Product_Category.product
